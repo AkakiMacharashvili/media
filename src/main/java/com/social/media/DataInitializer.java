90229a50f68345f599f706f3a8a896bc098b7f86
AkakiMacharashvili <akaki.macarashvili23@gmail.com>
2024-11-28 11:58:30 +0400

    Initial commit

---
+++ src/main/java/com/social/media/DataInitializer.java
@@ -0,0 +1,37 @@
+package com.social.media;
+
+import com.social.media.repositories.PostRepository;
+import com.social.media.repositories.SocialProfileRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.social.media.models.Post;
+import com.social.media.models.SocialGroup;
+import com.social.media.models.SocialProfile;
+import com.social.media.models.SocialUser;
+import com.social.media.repositories.SocialGroupRepository;
+import com.social.media.repositories.SocialUserRepository;
+
+@Configuration
+public class DataInitializer {
+
+    private final SocialUserRepository userRepository;
+    private final SocialGroupRepository groupRepository;
+    private final SocialProfileRepository socialProfileRepository;
+    private final PostRepository postRepository;
+
+    public DataInitializer(SocialUserRepository userRepository, SocialGroupRepository groupRepository, SocialProfileRepository socialProfileRepository, PostRepository postRepository) {
+        this.userRepository = userRepository;
+        this.groupRepository = groupRepository;
+        this.socialProfileRepository = socialProfileRepository;
+        this.postRepository = postRepository;
+    }
+
+    @Bean
+    public CommandLineRunner initializeData() {
+        return args -> {
+
+        };
+    }
+}
